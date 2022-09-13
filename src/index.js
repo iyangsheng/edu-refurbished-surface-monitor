17646455047c6d4fc3e775c469f3929f3d1bf5b5
iyangsheng <1648569746@qq.com>
2022-09-13 23:26:45 +0800

    chore(protocol): update api protocol

--- src/index.js
+++ src/index.js
@@ -23,11 +23,11 @@ function sleep(sleepTime) {
 
 function weixinNotify(key, logDesc, logDesp) {
   const data = {
-    text: logDesc,
+    title: logDesc,
     desp: logDesp,
   };
   const dataStr = querystring.stringify(data);
-  return axios.default.post(`https://sc.ftqq.com/${key}.send`, dataStr).then(res => res.data);
+  return axios.default.post(`https://sctapi.ftqq.com/${key}.send`, dataStr).then(res => res.data);
 }
 
 function formatDate(date, fmt) {
@@ -62,7 +62,7 @@ async function run(wantToBuyProductNameRegex = /Surface Go/, sizeRegex = /128GB/
       }
       console.group(formatDate(dateNow, 'yyyy/MM/dd HH:mm:ss fff : '));
       const result = await get(
-        `https://www.microsoftstore.com.cn/graphql?query=%7B+categoryList(filters%3A+%7Bids%3A+%7Bin%3A+%5B%2267%22%5D%7D%7D)+%7B+id+name+absolute_path+store+price_sort+products(pageSize%3A100)+%7B+total_count+items%7B+id+sku+name+image+%7B+label+url+%7D+private_price+qty_status+super_attribute+%7B+code+label+index+%7D+...+on+ConfigurableProduct+%7B+variants+%7B+attributes+%7B+code+label+value_index+%7D+product+%7B+id+sku+name+sub_name+image+%7B+label+url+%7D+private_price+qty_status+color+size+%7D+%7D+%7D+%7D+%7D+%7D+%7D&_=${Date.now()}`
+        `https://www.microsoftstore.com.cn/graphql?query=%7B+categoryList(filters%3A+%7Bids%3A+%7Bin%3A+%5B%2267%22%5D%7D%7D)+%7B+id+name+absolute_path+store+price_sort+products(pageSize%3A20+currentPage%3A1+sort%3A+%7Bposition%3AASC%7D)+%7B+total_count+items%7B+id+private_description+sku+name+marketing_txt+marketing_status+image+%7B+label+url+%7D+private_price+qty_status+super_attribute+%7B+code+label+index+%7D...+on+BundleProduct+%7B+url_key+dynamic_sku+dynamic_price+dynamic_weight+price_view+ship_bundle_items+items+%7B+title+required+type+main+bundle_type+position+sku+options+%7B+quantity+position+is_default+private_price+price_type+can_change_quantity+label+product+%7Bid+edu_bundle_message+qty_status+name+image+%7B+label+url+%7D+qty_status+sku+__typename%7D%7D%7D%7D...+on+ConfigurableProduct+%7B+variants+%7B+attributes+%7B+code+label+value_index+%7D+product+%7B+id+sku+private_description+name+sub_name+marketing_txt+marketing_status+image+%7B+label+url+%7D+private_price+qty_status+color+size+%7D+%7D+%7D+%7D+%7D+%7D+%7D&_=${Date.now()}`
       );
       const products = result.data.categoryList[0].products.items;
       if (products && products.length > 0) {
@@ -70,30 +70,31 @@ async function run(wantToBuyProductNameRegex = /Surface Go/, sizeRegex = /128GB/
         console.clear();
         let isAlreadyNotify = false;
         const productVariants = products
-          .map(x => x.variants)
+          .filter(item => item.sku !== '2571-00000')
+          .map(x => x.items[0].options)
           .reduce((acc, cur) => acc.concat(cur), [])
-          .filter(x => x.product.private_price && x.product.qty_status === 'true')
+          .filter(x => x.private_price && x.product.qty_status === 'true')
           .map(x => {
             return {
               ...x,
-              price: Number(x.product.private_price.replace('￥ ', '').replace(',', '')),
+              price: Number(x.private_price.replace('￥ ', '').replace(',', '')),
             };
           })
           .sort((a, b) => a.price - b.price);
-        productVariants.forEach(x => console.log(`${x.product.name} ${x.product.sub_name}  :   ${x.product.private_price}`));
+
+        productVariants.forEach(x => console.log(`${x.product.name}: ${x.private_price}`));
         if (LAST_PRODUCTS_VARIANTS.length > 0) {
-          const priceCutProducts = productVariants.filter(x =>
-            x.product.qty_status === 'true' &&
-            LAST_PRODUCTS_VARIANTS.some(old => old.product.name === x.product.name && old.product.sub_name === x.product.sub_name && x.price < old.price)
+          const priceCutProducts = productVariants.filter(
+            x => x.product.qty_status === 'true' && LAST_PRODUCTS_VARIANTS.some(old => old.product.sku === x.product.sku && x.price < old.price)
           );
           if (priceCutProducts.length > 0) {
-            let notifyMsg = `####  以下Surface 已降价：\r\n\r\n${priceCutProducts.map(x => `> ${x.product.name} ${x.product.sub_name} ${x.product.private_price}`).join('\r\n\r\n')}`; //`${wantToBuyProduct.name} 已到货:\r\n${canBuyProducts.map(x => `${x.product.sub_name} ${x.product.private_price}`).join('\r\n')}`;
-              console.group('notifyMsg : ');
-              console.log(notifyMsg);
-              console.groupEnd();
-              const notifyResult = await weixinNotify(argv.wxkey, '微软教育优惠降价通知', notifyMsg);
-              console.log(`notifyResult : `, notifyResult);
-              isAlreadyNotify = true;
+            let notifyMsg = `####  以下Surface 已降价：\r\n\r\n${priceCutProducts.map(x => `> ${x.product.name} ${x.private_price}`).join('\r\n\r\n')}`; //`${wantToBuyProduct.name} 已到货:\r\n${canBuyProducts.map(x => `${x.product.sub_name} ${x.product.private_price}`).join('\r\n')}`;
+            console.group('notifyMsg : ');
+            console.log(notifyMsg);
+            console.groupEnd();
+            const notifyResult = await weixinNotify(argv.wxkey, '微软教育优惠降价通知', notifyMsg);
+            console.log(`notifyResult : `, notifyResult);
+            isAlreadyNotify = true;
           }
         }
         LAST_PRODUCTS_VARIANTS = productVariants;
@@ -101,13 +102,13 @@ async function run(wantToBuyProductNameRegex = /Surface Go/, sizeRegex = /128GB/
         const wantToBuyProducts = products.filter(x => wantToBuyProductNameRegex.test(x.name));
         if (wantToBuyProducts && wantToBuyProducts.length > 0) {
           for (const wantToBuyProduct of wantToBuyProducts) {
-            if (wantToBuyProduct.variants && wantToBuyProduct.variants.length > 0) {
-              let canBuyProducts = wantToBuyProduct.variants.filter(x => x.product.qty_status === 'true');
+            if (wantToBuyProduct.items && wantToBuyProduct.items[0].options.length > 0) {
+              let canBuyProducts = wantToBuyProduct.items[0].options.filter(x => x.product.qty_status === 'true');
               if (canBuyProducts.length > 0 && sizeRegex) {
-                canBuyProducts = canBuyProducts.filter(x => sizeRegex.test(x.product.sub_name));
+                canBuyProducts = canBuyProducts.filter(x => sizeRegex.test(x.product.name));
               }
               if (canBuyProducts.length > 0) {
-                let notifyMsg = `####  ${wantToBuyProduct.name} 已到货：\r\n\r\n${canBuyProducts.map(x => `> ${x.product.sub_name} ${x.product.private_price}`).join('\r\n\r\n')}`; //`${wantToBuyProduct.name} 已到货:\r\n${canBuyProducts.map(x => `${x.product.sub_name} ${x.product.private_price}`).join('\r\n')}`;
+                let notifyMsg = `####  ${wantToBuyProduct.name} 已到货：\r\n\r\n${canBuyProducts.map(x => `> ${x.product.name} ${x.private_price}`).join('\r\n\r\n')}`; //`${wantToBuyProduct.name} 已到货:\r\n${canBuyProducts.map(x => `${x.product.sub_name} ${x.product.private_price}`).join('\r\n')}`;
                 console.group('notifyMsg : ');
                 console.log(notifyMsg);
                 console.groupEnd();
@@ -132,4 +133,4 @@ async function run(wantToBuyProductNameRegex = /Surface Go/, sizeRegex = /128GB/
   }
 }
 
-run(/Surface Go|Surface Pro 6/, /4415Y\/8GB\/128GB\/WiFi|i7\/16GB\/512GB/);
+run(/Surface Laptop 3/, /512GB/);
